8dbc171086db2401f96998f6bd7436a46efad18f
Molybdenum <547975141@qq.com>
2021-11-26 21:41:39 +0800

    add install

--- cmake/modules/WNTemplate.cmake
+++ cmake/modules/WNTemplate.cmake
@@ -6,6 +6,11 @@ macro(add_WNTemplate_tool name)
     if(TARGET ${name})
         # may install here
 
+        install(TARGETS ${name}
+                LIBRARY DESTINATION lib/${WNTemplate_LIBDIR_SUFFIX}
+                ARCHIVE DESTINATION lib/${WNTemplate_LIBDIR_SUFFIX}
+                RUNTIME DESTINATION bin
+                )
 
     else()
         add_custom_target(${name} ${ARGN})
@@ -19,11 +24,19 @@ macro(add_WNTemplate_library name)
 
         # may link here
 
-        install(TARGETS ${name}
+        install(TARGETS ${name} EXPORT ${name}-targets
                 COMPONENT ${name}
-                LIBRARY DESTINATION lib${WNTemplate_LIBDIR_SUFFIX}
-                ARCHIVE DESTINATION lib${WNTemplate_LIBDIR_SUFFIX}
-                RUNTIME DESTINATION bin)
+                LIBRARY DESTINATION lib/${WNTemplate_LIBDIR_SUFFIX}
+                ARCHIVE DESTINATION lib/${WNTemplate_LIBDIR_SUFFIX}
+                RUNTIME DESTINATION bin
+                INCLUDES DESTINATION include)
+
+        install(EXPORT ${name}-targets
+                FILE "${PROJECT_NAME}-${name}-targets.cmake"
+                NAMESPACE ${PROJECT_NAME}::
+                DESTINATION lib/cmake/${PROJECT_NAME}
+                COMPONENT ${component}
+                )
     else()
         add_custom_target(${name} ${ARGN})
     endif()
